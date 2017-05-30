6b2e7dc5d0b63f57436394a2f81368abad95b430
joon lee <jlee@skjline.local>
2017-05-30 11:59:01 -0400

    Update obsoleted references

--- android/src/androidTest/java/com/skjline/twitter/ExampleInstrumentedTest.java
+++ android/src/androidTest/java/com/skjline/twitter/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.exercise.twitter", appContext.getPackageName());
+        assertEquals("com.skjline.twitter", appContext.getPackageName());
     }
 }
